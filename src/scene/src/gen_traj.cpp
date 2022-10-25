a7c9709beba0409174a79a2a92de80f07aa34784
unsigned-long <3079625093@qq.com>
2022-10-25 20:42:07 +0800

    update

--- src/scene/src/gen_traj.cpp
+++ src/scene/src/gen_traj.cpp
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 #include "artwork/logger/logger.h"
 #include "pcl-viewer/scene_viewer.h"
+#include "tinyxml2.h"
 
 std::vector<std::string> splitString(const std::string &str, char splitor, bool ignoreEmpty = true) {
     std::vector<std::string> vec;
@@ -21,71 +22,132 @@ std::vector<std::string> splitString(const std::string &str, char splitor, bool 
     return vec;
 }
 
-std::string readString(std::fstream &file) {
-    if (!file.is_open())
-        throw std::runtime_error("file open failed in 'std::string readString(std::fstream &file)'");
-    file.seekg(0, std::ios::end);
-    auto size = file.tellg();
-    file.seekg(0, std::ios::beg);
-    std::string str(size, ' ');
-    file.read(const_cast<char *>(str.c_str()), size);
-    return str;
+std::vector<std::array<double, 6>> ReadPoseVecFromDEA(const std::string &str) {
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(str.c_str());
+    auto animation = doc.FirstChildElement()->FirstChildElement("library_animations")->FirstChildElement();
+    auto robot_location_X = animation->FirstChildElement();
+    auto robot_location_Y = robot_location_X->NextSiblingElement();
+    auto robot_location_Z = robot_location_Y->NextSiblingElement();
+
+    auto robot_rotation_euler_X = robot_location_Z->NextSiblingElement();
+    auto robot_rotation_euler_Y = robot_rotation_euler_X->NextSiblingElement();
+    auto robot_rotation_euler_Z = robot_rotation_euler_Y->NextSiblingElement();
+
+    auto GetDoubleArray = [](tinyxml2::XMLElement *element) {
+        auto aryElem = element->FirstChildElement()->NextSiblingElement()->FirstChildElement();
+        LOG_INFO(aryElem->FirstAttribute()->Value())
+        const std::string str = aryElem->GetText();
+        auto sVec = splitString(str, ' ');
+        std::vector<double> dVec(sVec.size());
+        for (int i = 0; i < sVec.size(); ++i) {
+            dVec.at(i) = std::stod(sVec.at(i));
+        }
+        return dVec;
+    };
+
+    auto xVec = GetDoubleArray(robot_location_X);
+    auto yVec = GetDoubleArray(robot_location_Y);
+    auto zVec = GetDoubleArray(robot_location_Z);
+
+    auto xEulerVec = GetDoubleArray(robot_rotation_euler_X);
+    auto yEulerVec = GetDoubleArray(robot_rotation_euler_Y);
+    auto zEulerVec = GetDoubleArray(robot_rotation_euler_Z);
+
+    LOG_VAR(xVec.size(), yVec.size(), zVec.size())
+    LOG_VAR(xEulerVec.size(), yEulerVec.size(), zEulerVec.size())
+
+    std::vector<std::array<double, 6>> ary(xVec.size());
+
+    for (int i = 0; i < xVec.size(); ++i) {
+        auto &elem = ary.at(i);
+        elem.at(0) = xVec.at(i);
+        elem.at(1) = yVec.at(i);
+        elem.at(2) = zVec.at(i);
+        elem.at(3) = xEulerVec.at(i);
+        elem.at(4) = yEulerVec.at(i);
+        elem.at(5) = zEulerVec.at(i);
+    }
+
+    return ary;
 }
 
-void SavePoseToSDF(const std::vector<ns_viewer::Posed> &poseSeq, const std::string &filename) {
-    std::ofstream file(filename, std::ios::out);
-    file << "<script>" << std::endl;
-    file << "<loop>1</loop>" << std::endl;
-    file << "<auto_start>1</auto_start>" << std::endl;
-    file << "<trajectory id='0' type='sim-traj'>" << std::endl;
-    for (const auto &pose: poseSeq) {
-        auto euler = pose.rotation.eulerAngles(0, 1, 2);
-        file << "<waypoint>" << std::endl;
-        file << "<time>" << pose.timeStamp << "</time>" << std::endl;
-        file << "<pose>"
-             << pose.translation(0) << ' '
-             << pose.translation(1) << ' '
-             << pose.translation(2) << ' '
-             << euler(0) << ' ' << euler(1) << ' ' << euler(2)
-             << "</pose>" << std::endl;
-        file << "</waypoint>" << std::endl;
-    }
-    file << "</trajectory>" << std::endl;
-    file << "</script>" << std::endl;
+tinyxml2::XMLNode *GetSimRobotScriptFromDEA(const std::string &filename, tinyxml2::XMLDocument &target) {
+    const auto poseSeq = ReadPoseVecFromDEA(filename);
 
-    file.close();
+    tinyxml2::XMLDocument doc;
+    static const double degToRad = M_PI / 180.0;
+
+    auto scriptElem = doc.NewElement("script");
+
+    auto loopElem = doc.NewElement("loop");
+    loopElem->InsertEndChild(doc.NewText("1"));
+    scriptElem->InsertEndChild(loopElem);
+
+    auto autoStartElem = doc.NewElement("auto_start");
+    autoStartElem->InsertEndChild(doc.NewText("1"));
+    scriptElem->InsertEndChild(autoStartElem);
+
+    auto trajectoryElem = doc.NewElement("trajectory");
+    trajectoryElem->SetAttribute("id", 0);
+    trajectoryElem->SetAttribute("type", "sim-traj");
+    for (int i = 0; i < poseSeq.size(); ++i) {
+        const auto &pose = poseSeq.at(i);
+        const double timeStamp = i / 10.0;
+        auto waypointElem = doc.NewElement("waypoint");
+
+        auto timeElem = doc.NewElement("time");
+        timeElem->InsertEndChild(doc.NewText(std::to_string(timeStamp).c_str()));
+        waypointElem->InsertEndChild(timeElem);
+
+        auto poseElem = doc.NewElement("pose");
+        std::stringstream stream;
+        stream << pose.at(0) << ' '
+               << pose.at(1) << ' '
+               << pose.at(2) << ' '
+               << pose.at(3) * degToRad << ' '
+               << pose.at(4) * degToRad << ' '
+               << pose.at(5) * degToRad;
+        poseElem->InsertEndChild(doc.NewText(stream.str().c_str()));
+        waypointElem->InsertEndChild(poseElem);
+
+        trajectoryElem->InsertEndChild(waypointElem);
+    }
+    scriptElem->InsertEndChild(trajectoryElem);
+
+    return scriptElem->DeepClone(&target);
+}
+
+tinyxml2::XMLNode *GetSimRobotLinkFromSDF(const std::string &filename, tinyxml2::XMLDocument &target) {
+    tinyxml2::XMLDocument robot;
+    robot.LoadFile(filename.c_str());
+    auto linkElem = robot.FirstChild()->FirstChild()->FirstChild();
+    return linkElem->DeepClone(&target);
 }
 
 int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "gen_traj");
-    std::fstream file("/home/csl/ros_ws/sim-scene/src/scene/model/trajectory/trajectory.txt", std::ios::in);
-    auto str = readString(file);
-    file.close();
+    ros::init(argc, argv, "generate_actor");
+    LOG_PROCESS("start reading trajectory dea file")
 
-    auto sVec = splitString(str, ' ');
-    std::vector<double> dVec(sVec.size());
-    for (int i = 0; i < sVec.size(); ++i) {
-        dVec.at(i) = std::stod(sVec.at(i));
-    }
+    tinyxml2::XMLDocument doc;
 
-    const int POSE_SIZE = dVec.size() / 16;
-    const double SCALE_FACTOR = 0.15;
+    auto actorElem = doc.NewElement("actor");
+    actorElem->SetAttribute("name", "sim_robot");
 
-    ns_viewer::SceneViewer viewer;
-    std::vector<ns_viewer::Posed> pose(POSE_SIZE);
-    for (int i = 0; i < POSE_SIZE; ++i) {
-        Eigen::Matrix4d mat;
-        mat << dVec.at(i * 16 + 0), dVec.at(i * 16 + 1), dVec.at(i * 16 + 2), dVec.at(i * 16 + 3),
-                dVec.at(i * 16 + 4), dVec.at(i * 16 + 5), dVec.at(i * 16 + 6), dVec.at(i * 16 + 7),
-                dVec.at(i * 16 + 8), dVec.at(i * 16 + 9), dVec.at(i * 16 + 10), dVec.at(i * 16 + 11),
-                dVec.at(i * 16 + 12), dVec.at(i * 16 + 13), dVec.at(i * 16 + 14), dVec.at(i * 16 + 15);
-        mat.topLeftCorner<3, 3>() /= SCALE_FACTOR;
-        pose.at(i) = ns_viewer::Posed(mat, i / 10.0);
-        viewer.AddPose(pose.at(i).cast<float>());
-        LOG_VAR(i, mat);
-    }
-    viewer.RunSingleThread();
-    SavePoseToSDF(pose, "/home/csl/ros_ws/sim-scene/src/scene/model/trajectory/trajectory.xml");
+    auto linkElem = GetSimRobotLinkFromSDF(
+            "/home/csl/ros_ws/sim-scene/src/scene/model/sdf/sim-robot.sdf", doc
+    );
+    actorElem->InsertEndChild(linkElem);
+
+    auto scriptElem = GetSimRobotScriptFromDEA(
+            "/home/csl/ros_ws/sim-scene/src/scene/model/trajectory/trajectory.dae", doc);
+    actorElem->InsertEndChild(scriptElem);
+
+    doc.InsertEndChild(actorElem);
+
+    doc.SaveFile("/home/csl/ros_ws/sim-scene/src/scene/model/sdf/actor.sdf");
+
+    LOG_PROCESS("save trajectory dea to file")
     ros::shutdown();
     return 0;
 }
