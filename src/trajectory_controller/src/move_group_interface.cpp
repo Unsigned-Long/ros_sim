6ab81b25fb23d1bd21d5a893ad66309dedbd51c4
unsigned-long <3079625093@qq.com>
2022-10-30 17:28:41 +0800

    add traj controller

--- src/trajectory_controller/src/move_group_interface.cpp
+++ src/trajectory_controller/src/move_group_interface.cpp
@@ -49,159 +49,160 @@
 const double tau = 2 * M_PI;
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "move_group_interface_tutorial");
-  ros::NodeHandle node_handle;
+    ros::init(argc, argv, "move_group_interface_tutorial");
+    ros::NodeHandle node_handle;
 
-  // ROS spinning must be running for the MoveGroupInterface to get information
-  // about the robot's state. One way to do this is to start an AsyncSpinner
-  // beforehand.
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+    // ROS spinning must be running for the MoveGroupInterface to get information
+    // about the robot's state. One way to do this is to start an AsyncSpinner
+    // beforehand.
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
-  // BEGIN_TUTORIAL
-  //
-  // Setup
-  // ^^^^^
-  //
-  // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
-  // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
-  // are used interchangeably.
-  static const std::string PLANNING_GROUP = "arm";
+    // BEGIN_TUTORIAL
+    //
+    // Setup
+    // ^^^^^
+    //
+    // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
+    // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
+    // are used interchangeably.
+    static const std::string PLANNING_GROUP = "arm_robot";
 
-  // The :planning_interface:`MoveGroupInterface` class can be easily
-  // setup using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+    // The :planning_interface:`MoveGroupInterface` class can be easily
+    // setup using just the name of the planning group you would like to control and plan for.
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
 
-  // We will use the :planning_interface:`PlanningSceneInterface`
-  // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    // We will use the :planning_interface:`PlanningSceneInterface`
+    // class to add and remove collision objects in our "virtual world" scene
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const moveit::core::JointModelGroup *joint_model_group =
-      move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+    // Raw pointers are frequently used to refer to the planning group for improved performance.
+    const moveit::core::JointModelGroup *joint_model_group =
+            move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  // Visualization
-  // ^^^^^^^^^^^^^
-  //
-  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("link5");
-  visual_tools.deleteAllMarkers();
+    // Visualization
+    // ^^^^^^^^^^^^^
+    //
+    // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
+    // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
+    namespace rvt = rviz_visual_tools;
+    moveit_visual_tools::MoveItVisualTools visual_tools("base");
+    visual_tools.deleteAllMarkers();
 
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in RViz
-  visual_tools.loadRemoteControl();
+    // Remote control is an introspection tool that allows users to step through a high level script
+    // via buttons and keyboard shortcuts in RViz
+    visual_tools.loadRemoteControl();
 
-  // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-  text_pose.translation().z() = 1.0;
-  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+    // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
+    Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+    text_pose.translation().z() = 1.0;
+    visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
 
-  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
-  visual_tools.trigger();
+    // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
+    visual_tools.trigger();
 
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // We can print the name of the reference frame for this robot.
-  ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
+    // Getting Basic Information
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^
+    //
+    // We can print the name of the reference frame for this robot.
+    ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
 
-  // We can also print the name of the end-effector link for this group.
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
+    // We can also print the name of the end-effector link for this group.
+    ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
 
-  // We can get a list of all the groups in the robot:
-  ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
-  std::copy(move_group_interface.getJointModelGroupNames().begin(),
-            move_group_interface.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
+    // We can get a list of all the groups in the robot:
+    ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+    std::copy(move_group_interface.getJointModelGroupNames().begin(),
+              move_group_interface.getJointModelGroupNames().end(),
+              std::ostream_iterator<std::string>(std::cout, ", "));
 
-  // Start the demo
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+    // Start the demo
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
-  // .. _move_group_interface-planning-to-pose-goal:
-  //
-  // Planning to a Pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^
-  // We can plan a motion for this group to a desired pose for the
-  // end-effector.
-  geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.w = 1.0;
-  target_pose1.position.x = 1;
-  target_pose1.position.y = 1;
-  target_pose1.position.z = 1;
-  move_group_interface.setPoseTarget(target_pose1);
+    // .. _move_group_interface-planning-to-pose-goal:
+    //
+    // Planning to a Pose goal
+    // ^^^^^^^^^^^^^^^^^^^^^^^
+    // We can plan a motion for this group to a desired pose for the
+    // end-effector.
+    geometry_msgs::Pose target_pose1;
+    target_pose1.orientation.w = 1.0;
+    target_pose1.position.x = 1;
+    target_pose1.position.y = 1;
+    target_pose1.position.z = 1;
+    move_group_interface.setPoseTarget(target_pose1);
 
-  // Moving to a pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^
-  //
-  // If you do not want to inspect the planned trajectory,
-  // the following is a more robust combination of the two-step plan+execute pattern shown above
-  // and should be preferred. Note that the pose goal we had set earlier is still active,
-  // so the robot will try to move to that goal.
+    // Moving to a pose goal
+    // ^^^^^^^^^^^^^^^^^^^^^
+    //
+    // If you do not want to inspect the planned trajectory,
+    // the following is a more robust combination of the two-step plan+execute pattern shown above
+    // and should be preferred. Note that the pose goal we had set earlier is still active,
+    // so the robot will try to move to that goal.
 
-  // TODO: try this
-  // move_group_interface.setPathConstraints()
-  move_group_interface.move();
+    // TODO: try this
+    // move_group_interface.setPathConstraints()
+    move_group_interface.move();
 
-  target_pose1.position.x = 2;
-  target_pose1.position.y = -2;
-  target_pose1.position.z = 2;
-  move_group_interface.setPoseTarget(target_pose1);
+    target_pose1.position.x = 2;
+    target_pose1.position.y = -2;
+    target_pose1.position.z = 2;
+    move_group_interface.setPoseTarget(target_pose1);
 
-  move_group_interface.move();
+    move_group_interface.move();
 
-  visual_tools.prompt("finished");
+    visual_tools.prompt("finished");
 
-  // Cartesian Paths
-  // ^^^^^^^^^^^^^^^
-  // You can plan a Cartesian path directly by specifying a list of waypoints
-  // for the end-effector to go through. Note that we are starting
-  // from the new start state above.  The initial pose (start state) does not
-  // need to be added to the waypoint list but adding it can help with visualizations
-  // std::vector<geometry_msgs::Pose> waypoints;
-  // waypoints.push_back(start_pose2);
+    // Cartesian Paths
+    // ^^^^^^^^^^^^^^^
+    // You can plan a Cartesian path directly by specifying a list of waypoints
+    // for the end-effector to go through. Note that we are starting
+    // from the new start state above.  The initial pose (start state) does not
+    // need to be added to the waypoint list but adding it can help with visualizations
+    // std::vector<geometry_msgs::Pose> waypoints;
+    // waypoints.push_back(start_pose2);
 
-  // geometry_msgs::Pose target_pose3 = start_pose2;
+    // geometry_msgs::Pose target_pose3 = start_pose2;
 
-  // target_pose3.position.z -= 0.2;
-  // waypoints.push_back(target_pose3);  // down
+    // target_pose3.position.z -= 0.2;
+    // waypoints.push_back(target_pose3);  // down
 
-  // target_pose3.position.y -= 0.2;
-  // waypoints.push_back(target_pose3);  // right
+    // target_pose3.position.y -= 0.2;
+    // waypoints.push_back(target_pose3);  // right
 
-  // target_pose3.position.z += 0.2;
-  // target_pose3.position.y += 0.2;
-  // target_pose3.position.x -= 0.2;
-  // waypoints.push_back(target_pose3);  // up and left
+    // target_pose3.position.z += 0.2;
+    // target_pose3.position.y += 0.2;
+    // target_pose3.position.x -= 0.2;
+    // waypoints.push_back(target_pose3);  // up and left
 
-  // We want the Cartesian path to be interpolated at a resolution of 1 cm
-  // which is why we will specify 0.01 as the max step in Cartesian
-  // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
-  // Warning - disabling the jump threshold while operating real hardware can cause
-  // large unpredictable motions of redundant joints and could be a safety issue
-  // moveit_msgs::RobotTrajectory trajectory;
-  // const double jump_threshold = 0.0;
-  // const double eef_step = 0.01;
-  // double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
-  // ROS_INFO_NAMED("tutorial", "Visualizing plan 4 (Cartesian path) (%.2f%% achieved)", fraction * 100.0);
-  //     // Visualize the plan in RViz
-  //     visual_tools.deleteAllMarkers();
-  // visual_tools.publishText(text_pose, "Cartesian Path", rvt::WHITE, rvt::XLARGE);
-  // visual_tools.publishPath(waypoints, rvt::LIME_GREEN, rvt::SMALL);
-  // for (std::size_t i = 0; i < waypoints.size(); ++i)
-  //   visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::SMALL);
-  // visual_tools.trigger();
-  // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+    // We want the Cartesian path to be interpolated at a resolution of 1 cm
+    // which is why we will specify 0.01 as the max step in Cartesian
+    // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
+    // Warning - disabling the jump threshold while operating real hardware can cause
+    // large unpredictable motions of redundant joints and could be a safety issue
+    // moveit_msgs::RobotTrajectory trajectory;
+    // const double jump_threshold = 0.0;
+    // const double eef_step = 0.01;
+    // double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+    // ROS_INFO_NAMED("tutorial", "Visualizing plan 4 (Cartesian path) (%.2f%% achieved)", fraction * 100.0);
+    //     // Visualize the plan in RViz
+    //     visual_tools.deleteAllMarkers();
+    // visual_tools.publishText(text_pose, "Cartesian Path", rvt::WHITE, rvt::XLARGE);
+    // visual_tools.publishPath(waypoints, rvt::LIME_GREEN, rvt::SMALL);
+    // for (std::size_t i = 0; i < waypoints.size(); ++i)
+    //   visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::SMALL);
+    // visual_tools.trigger();
+    // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
-  // Cartesian motions should often be slow, e.g. when approaching objects. The speed of cartesian
-  // plans cannot currently be set through the maxVelocityScalingFactor, but requires you to time
-  // the trajectory manually, as described `here <https://groups.google.com/forum/#!topic/moveit-users/MOoFxy2exT4>`_.
-  // Pull requests are welcome.
-  //
-  // You can execute a trajectory like this.
-  // move_group_interface.execute(trajectory);
+    // Cartesian motions should often be slow, e.g. when approaching objects. The speed of cartesian
+    // plans cannot currently be set through the maxVelocityScalingFactor, but requires you to time
+    // the trajectory manually, as described `here <https://groups.google.com/forum/#!topic/moveit-users/MOoFxy2exT4>`_.
+    // Pull requests are welcome.
+    //
+    // You can execute a trajectory like this.
+    // move_group_interface.execute(trajectory);
 
-  ros::shutdown();
-  return 0;
+    ros::shutdown();
+    return 0;
 }
